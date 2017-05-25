d2d4ff28d71b0ae67e6789d9bb04e3b24aab692c
MikeMatt16 <mike.matt16@gmail.com>
2017-05-25 03:17:48 -0400

    Prepared Abide project.

--- Abide/Abide/Main.cs
+++ Abide/Abide/Main.cs
@@ -9,9 +9,9 @@ using System.Windows.Forms;
 
 namespace Abide
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
